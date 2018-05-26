7deba4a19c5feab486d51a7ad1b655c87772170b
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-26 14:18:20 -0700

    Added comment on slugs

--- personalblog/blog/models.py
+++ personalblog/blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     # Defines the different characteristics of the elements
     # of the model
     title = models.CharField(max_length=100, unique=True)
+    # A slug is the end bit of a url. ex: google.com/search,
+    # search is the slug
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
