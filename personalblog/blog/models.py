bedafe9165b77d675be51f23cbe4fedca0a34531
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-26 14:16:37 -0700

    Create Post Model

--- personalblog/blog/models.py
+++ personalblog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # Defines the different characteristics of the elements
+    # of the model
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    posted = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('blog.Category')
