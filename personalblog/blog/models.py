c1700dfea73a0a13ef70673fad355bbc8fccd166
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-26 14:47:39 -0700

    Added unicode and get_absolute_url methods to the Post Model

--- personalblog/blog/models.py
+++ personalblog/blog/models.py
@@ -12,6 +12,13 @@ class Post(models.Model):
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('blog.Category')
 
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ("view_blog_post", None, { 'slug': self.slug})
+
 class Category(models.Model)
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
