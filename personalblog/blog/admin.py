b6e3805767be26f0049691e6278bb7860f835e5d
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-26 19:23:01 -0700

    Add BlogAdmin class in admin.py

--- personalblog/blog/admin.py
+++ personalblog/blog/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 # You can access any .py file this way, atleast in django
 from blog.models import Post, Category
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    exclude = ['posted']
+    prepopulated_fields = { 'slug': {'title'}}
