5f02688fbd9e27d70f3a730f9204b7bee4d31e9e
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-26 19:39:04 -0700

    Coded index request and started view_post request

--- personalblog/blog/views.py
+++ personalblog/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
 from personalblog.blog.models import Post, Category
 # Create your views here.
+
+def index(request):
+    # This index request only loads in 5 at a time
+    return render_to_response('index.html', {
+        'categories': Category.objects.all(),
+        'posts': Post.objects.all()[:5]
+    })
+
+def view_post(request):
+    return render_to_response('view_post.html', {
+    
+    })
