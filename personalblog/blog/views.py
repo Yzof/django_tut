93766bfbf0f9f2d98455b15b61628a560e16253c
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-28 16:52:33 -0700

    Changed import from personalblog.blog to simply just blog

--- personalblog/blog/views.py
+++ personalblog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
-from personalblog.blog.models import Post, Category
+from blog.models import Post, Category
 # Create your views here.
 
 def index(request):
