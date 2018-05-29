4a8635d2ef7c5e63bf714c9284d1841f0f186af2
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-28 22:38:54 -0700

    Registered PostAdmin and CategoryAdmin. Changed title into proper tuple

--- personalblog/blog/admin.py
+++ personalblog/blog/admin.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 from blog.models import Post, Category
 # Register your models here.
 
-class BlogAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     # This will actually let posted be self-filled, excluding it from
     # user/admin control
-    exclude = ['posted']
+    exclude = ['posted',]
     # This prepopulated fields option will fill in the slug for us
-    prepopulated_fields = { 'slug': {'title'}}
+    prepopulated_fields = { 'slug': ('title',),}
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': {'title'}}
+    prepopulated_fields = {'slug': ('title',),}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
